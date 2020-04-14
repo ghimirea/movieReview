29f92401ac6443bd84901471bae1c86cc9f719c3
Moe-Hassan123 <mohamedalfidil123@gmail.com>
2020-04-14 13:26:27 -0700

    Moh-Update

--- ams-theater/src/container/NavBar/nav.js
+++ ams-theater/src/container/NavBar/nav.js
@@ -3,101 +3,112 @@ import { Route, Link, Switch, Redirect } from "react-router-dom";
 import ImageRab from "../Imag/imgControler";
 import importComponent from "../../HOC/asynchComponent";
 import logo from "../../moviejpg.jpg";
-import Login from "../SignInSignUp/Login"
+import Login from "../SignInSignUp/Login";
+import Com from "../movie/contSingal";
 
-import { connect } from "react-redux"
+import { connect } from "react-redux";
 import Singal from "../movie/singalMoview";
 // import Login from "../SignInSignUp/Login";
 
-
 const asycNewmovie = importComponent(() => {
-    return import("../NewMovie/NewMovie");
+  return import("../NewMovie/NewMovie");
 });
 
-
 class Nav extends Component {
+  render() {
+    this.props.adminfunc();
+    return (
+      <div>
+        <header>
+          <nav className="   btn btn-warning disabled navbar navbar-expand-lg navbar-dark bg-dark">
+            <Link class=" text-warning navbar-brand" to="/">
+              {/* {logo} */}
+              <img
+                src="https://cdn2.iconfinder.com/data/icons/picons-basic-3/57/basic3-055_video_recording_hd-512.png"
+                width="30"
+                height="30"
+                alt="movies.com"
+              />{" "}
+              Movies-4YOU
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarColor02"
+              aria-controls="navbarColor02"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
 
-    render() {
-        this.props.adminfunc()
-        return (
-            <div>
-
-                <header>
-
-                    <nav className="   btn btn-warning disabled navbar navbar-expand-lg navbar-dark bg-dark" >
-                        <Link class=" text-warning navbar-brand" to="/">
-                            {/* {logo} */}
-                            <img src="https://cdn2.iconfinder.com/data/icons/picons-basic-3/57/basic3-055_video_recording_hd-512.png" width="30" height="30" alt="movies.com" />{" "}
-                             Movies-4YOU</Link>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div className=" container collapse navbar-collapse" id="navbarColor02">
-                            <ul className="navbar-nav mr-auto">
-                                <li className="nav-item active">
-                                    <Link className="nav-link" to="/"><span class="sr-only">(current)</span></Link>
-                                </li>
-                                {this.props.adminLoging === true ? <li className="nav-item">
-                                    <Link className="nav-link" to="/add-new-movie"> ADD-New-Movies </Link>
-                                </li> : null}
-                                <li className="nav-item">
-                                    <Link className="nav-link" to=" ">TOP-Movies</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="" >About</Link>
-                                </li>
-                            </ul>
-
-                        </div>
-                        <div className="collapse navbar-collapse">
-                            <Link className="btn btn-outline-success" to="/Singup" >Singup</Link>
-                            <Link className=" btn btn-outline-success" to="/Login" >Login</Link>
-                        </div>
-                    </nav>
-
-
-                    {
-                        <Switch>
-
-
-
-                            <Route exact path="/" component={ImageRab} />;
-                        <Route exact path="/add-new-movie" component={asycNewmovie} />;
-
-                        <Route exact path="/Ima/" component={Singal} />;
-                        <Route exact path="/Login" component={Login} />;
-                            <Route exact path="/Ima/" component={Singal} />
-
-
-                        </Switch>
-
-                    }
-                </header>
-
+            <div
+              className=" container collapse navbar-collapse"
+              id="navbarColor02"
+            >
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <Link className="nav-link" to="/">
+                    <span class="sr-only">(current)</span>
+                  </Link>
+                </li>
+                {this.props.adminLoging === true ? (
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/add-new-movie">
+                      {" "}
+                      ADD-New-Movies{" "}
+                    </Link>
+                  </li>
+                ) : null}
+                <li className="nav-item">
+                  <Link className="nav-link" to=" ">
+                    TOP-Movies
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="">
+                    About
+                  </Link>
+                </li>
+              </ul>
             </div>
-        );
-    }
+            <div className="collapse navbar-collapse">
+              <Link className="btn btn-outline-success" to="/Singup">
+                Singup
+              </Link>
+              <Link className=" btn btn-outline-success" to="/Login">
+                Login
+              </Link>
+            </div>
+          </nav>
 
-
-
-
-
+          {
+            <Switch>
+              <Route exact path="/" component={ImageRab} />;
+              <Route exact path="/add-new-movie" component={asycNewmovie} />;
+              <Route exact path="/Login" component={Login} />;
+              <Route exact path="/Ima/:id" component={Com} />
+            </Switch>
+          }
+        </header>
+      </div>
+    );
+  }
 }
 
 const mapstateToProps = (state) => {
-    return {
-        adminLoging: state.admin.admin,
-    };
+  return {
+    adminLoging: state.admin.admin,
+  };
 };
 
 const mapstateDispatch = (dispatch) => {
-    return {
-        adminfunc: () => {
-            dispatch({ type: "loginig" });
-        },
-    };
+  return {
+    adminfunc: () => {
+      dispatch({ type: "loginig" });
+    },
+  };
 };
-
 
 export default connect(mapstateToProps, mapstateDispatch)(Nav);
--- ams-theater/src/container/movie/singalMoview.js
+++ ams-theater/src/container/movie/singalMoview.js
@@ -1,27 +1,23 @@
-import React from "react"
-import { withRouter } from "react-router"
-import Com from "../coment/com"
+import React from "react";
+import { withRouter } from "react-router";
+import Com from "../movie/contSingal";
 import "./s.css";
 
-
 const Singale = (props) => {
-    // console.log(props)
-    console.log("from Singal")
-    return (
-        <div className="container " >
-            <div className="ge " >
-                <img className="   card-img-top" src="https://www.joblo.com/assets/images/joblo/posters/2019/01/IO-poster-1.jpg" />
-                <div className="card-body">
-                    <h5 className="card-title">title</h5>
-                    <p className="card-text">Some quick example</p>
-                </div>
-            </div>
-            <div>  <Com /> </div>
+  console.log(props);
+  console.log("from Singal");
 
+  return (
+    <div className="container ">
+      <div className="ge ">
+        <img className=" card-img-top" src={props.Mimg} />
+        <div className="card-body">
+          <h5 className="card-title">title {props.Mtitle}</h5>
+          <p className="card-text">Type {props.Mtype}</p>
         </div>
-    )
+      </div>
+    </div>
+  );
+};
 
-}
-
-
-export default Singale
+export default Singale;
