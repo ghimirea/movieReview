8d9c8f838a0c018f5f1c3dac16e64d9441e44afb
selambs <sshibru@mum.edu>
2020-04-15 02:49:59 -0500

    .

--- ams-theater/src/container/NewMovie/NewMovie.js
+++ ams-theater/src/container/NewMovie/NewMovie.js
@@ -36,10 +36,6 @@ class NewMovie extends Component {
 
     axios.post("/add-new-movie", newMovie).then((data) => {
       console.log(data);
-      // console.log(this.state);
-      // this.setState({
-      //   newCreated: true,
-      // });
       this.props.history.replace("/");
     });
   };
